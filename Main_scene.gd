5a684f991c493d8e2fa875d9cc001797c262bb95
https://github.com/kawaii-flesh/WYSAR <kawaiiflesh@yandex.ru>
2020-06-19 00:23:22 +0500

    Add text/name and bg commands

--- Main_scene.gd
+++ Main_scene.gd
@@ -4,7 +4,7 @@ var wc = load("res://WYSAR_Character.gd")
 var w = load("res://WYSAR.gd")
 
 var tex = ["res://texts/0"]
-var bgs = ["res://bgs/bg.png"]
+var bgs = ["res://bgs/bg.png", "res://bgs/bg1.jpg"]
 var fonts = ["res://fonts/0.ttf"]
 var sn = ["res://fonts/0.ttf"]
 var ms = ["res://fonts/0.ttf"]
@@ -12,5 +12,5 @@ var ch = [WYSAR_Character.new(["res://MCH/1.png"])]
 
 var wysar = WYSAR.new("res://texts/0.tres", tex, bgs, fonts, sn, ms, ch)
 
-func _ready():
+func _ready():	
 	self.add_child(wysar)
--- WYSAR.gd
+++ WYSAR.gd
@@ -13,14 +13,14 @@ var text_str: String
 var name_str: String
 var text_fnt: DynamicFont = DynamicFont.new()
 var name_fnt: DynamicFont = DynamicFont.new()
-var text_pos: Vector2 = Vector2(100, 100)
+var text_pos: Vector2 = Vector2.ZERO
 var name_pos: Vector2 = Vector2.ZERO
 var text_clr: Color = Color("000000")
 var name_clr: Color = Color("000000")
-var text_csh: int = 16
-var text_csv: int = text_csh + 6
-var name_csh: int = 16
-var delay_text: float = 100.0
+var text_csh: int = 0
+var text_csv: int = 0
+var name_csh: int = 0
+var delay_text: float = 0.0
 var vis_text: bool = true
 var vis_name: bool = true
 
@@ -34,6 +34,13 @@ var sound_list: PoolStringArray = []
 var music_list: PoolStringArray = []
 var character_list: = []
 
+var bg_b: bool = false
+var bg_l: bool = false
+var bg_c: bool = false
+var bgsp: float = 0.0
+var bgid: int = 0
+var bgm: Color = Color(1.0, 1.0, 1.0, 1.0)
+
 func _init(stf: String, tex: PoolStringArray, bgs: PoolStringArray, fnts: PoolStringArray, sn: PoolStringArray, ms: PoolStringArray, cha):
 	self.text_list = tex
 	self.bg_list = bgs
@@ -41,25 +48,19 @@ func _init(stf: String, tex: PoolStringArray, bgs: PoolStringArray, fnts: PoolSt
 	self.sound_list = sn
 	self.music_list = ms
 	self.character_list = cha
-	print(position)
 	for i in self.character_list:
 		self.add_child(i)
-	self.text_fnt.font_data = load("res://fonts/0.ttf")
-	self.text_fnt.size = 24
-	self.bg.texture = load("res://bgs/bg.png")
 	self.bg.centered = false
-	open_file(stf)
+	open_file(stf)	
 	
 	print("Constructed!")
 
-
 func read_file():
-	var ch = current_file_text[current_pos_in_file]	
+	if current_pos_in_file >= current_file_text.length():
+		return ''
+	var ch = current_file_text[current_pos_in_file]
 	self.current_pos_in_file += 1
 	return ch
-
-func command_section():
-	pass
 	
 func delay(t: float):
 	var time = OS.get_ticks_msec()
@@ -74,34 +75,46 @@ func open_file(nf):
 var time = 0.0
 
 func _input(event):
-	if event is InputEventKey and event.scancode == KEY_ENTER:
+	if Input.is_key_pressed(KEY_ENTER):
 		wait_enter = false
 		if clear_text:
 			text_str = ""
-			
+		
 func _process(delta):
+	if bg_b or bg_l:
+		update()
+		return
 	var dt = delay_text
-	if Input.is_key_pressed(KEY_SPACE):
-		dt /= 2
+	if Input.is_key_pressed(KEY_SPACE) and not Input.is_key_pressed(KEY_ENTER):
+		dt /= 3
 	delay(dt)
 	if wait_enter:		
-		return
+		return	
 	var symb = self.read_file()
+	if symb == '': return
 	
 	if symb == ';':
 		self.wait_enter = true
 		self.clear_text = true
+		if current_file_text[current_pos_in_file] == '\n':
+			current_pos_in_file += 1
 	elif symb == '$':
 		self.wait_enter = true
+		if current_file_text[current_pos_in_file] == '\n':
+			current_pos_in_file += 1
 	elif symb == '~':
 		delay(dt)
+		if current_file_text[current_pos_in_file] == '\n':
+			current_pos_in_file += 1
 	elif symb == '^':
 		self.text_str = ""
+		if current_file_text[current_pos_in_file] == '\n':
+			current_pos_in_file += 1
 	elif symb == '\\':
 		symb = self.read_file()
 		self.text_str += symb
 	elif symb == '|':
-		command_section() # TODO
+		command_section()
 	else:
 		self.text_str += symb
 	
@@ -109,10 +122,10 @@ func _process(delta):
 
 func draw_text():
 	var pos = self.text_pos
-	for i in self.text_str:		
+	for i in self.text_str:
 		draw_string(self.text_fnt, pos, i, self.text_clr)
 		if i == '\n':
-			pos.y += self.text_csh + 5
+			pos.y += self.text_csv
 			pos.x = self.text_pos.x
 		else:
 			pos.x += self.text_csh
@@ -121,6 +134,204 @@ func draw_name():
 	draw_string(self.name_fnt, self.name_pos, self.name_str, self.name_clr)
 	
 func _draw():
-	draw_texture(self.bg.texture, Vector2.ZERO)
-	draw_text()
-	draw_name()
+	if self.bg_b == false and self.bg_l == true and self.bg_c == true:
+		self.bg.set_texture(load(bg_list[bgid]))
+		self.bg_c = false
+	if bg_b:
+		delay(bgsp)
+		if self.bgm.r > 0:
+			self.bgm.r -= 0.05
+			self.bgm.g -= 0.05
+			self.bgm.b -= 0.05
+		else:
+			self.bg_b = false
+			self.bgm = Color(0, 0, 0, 1)
+		draw_texture(self.bg.texture, Vector2.ZERO, self.bgm)	
+	elif self.bg_l:
+		delay(self.bgsp)
+		if self.bgm.r < 1.0:
+			self.bgm.r += 0.05
+			self.bgm.g += 0.05
+			self.bgm.b += 0.05
+		else:
+			self.bg_l = false
+			self.bgm = Color(1, 1, 1, 1)
+		draw_texture(self.bg.texture, Vector2.ZERO, self.bgm)
+	else:
+		draw_texture(self.bg.texture, Vector2.ZERO, self.bgm)
+	if not self.bg_b and not self.bg_l:
+		draw_text()
+		draw_name()
+
+func command_section():	
+	var ch = self.read_file()
+	var com_id = ""
+	while ch != '|':
+		com_id += ch
+		ch = self.read_file()
+	
+	if com_id == "0":		
+		com_id = ""
+		ch = self.read_file()
+		while ch != '|':
+			com_id += ch
+			ch = self.read_file()
+		if com_id == '0':
+			var x = ""
+			var y = ""
+			ch = self.read_file()
+			while ch != ',':
+				x += ch
+				ch = self.read_file()
+			while ch != '|':
+				y += ch
+				ch = self.read_file()
+			self.text_pos.x = int(x)
+			self.text_pos.y = int(y)
+		elif com_id == '1':
+			var x = ""
+			var y = ""
+			ch = self.read_file()
+			while ch != ',':
+				x += ch
+				ch = self.read_file()
+			while ch != '|':
+				y += ch
+				ch = self.read_file()
+			self.name_pos.x = int(x)
+			self.name_pos.y = int(y)
+		elif com_id == '2': #argb
+			var nclr = ""
+			ch = self.read_file()
+			while ch != '|':
+				nclr += ch
+				ch = self.read_file()
+			self.text_clr = Color(nclr)
+		elif com_id == '3':
+			var nclr = ""
+			ch = self.read_file()
+			while ch != '|':
+				nclr += ch
+				ch = self.read_file()
+			self.name_clr = Color(nclr)
+		elif com_id == '4':
+			var f_id = ""
+			var f_s = ""
+			ch = self.read_file()
+			while ch != ',':
+				f_id += ch
+				ch = self.read_file()
+			while ch != '|':
+				f_s += ch
+				ch = self.read_file()
+			self.text_fnt.set_font_data(load(fonts_list[int(f_id)]))
+			self.text_fnt.set_size(int(f_s))
+		elif com_id == '5':
+			var f_id = ""
+			var f_s = ""
+			ch = self.read_file()
+			while ch != ',':
+				f_id += ch
+				ch = self.read_file()
+			while ch != '|':
+				f_s += ch
+				ch = self.read_file()
+			self.name_fnt.set_font_data(load(fonts_list[int(f_id)]))
+			self.name_fnt.set_size(int(f_s))
+		elif com_id == '6':
+			var t = ""
+			ch = self.read_file()
+			while ch != '|':
+				t += ch
+				ch = self.read_file()
+			self.text_str = t
+		elif com_id == '7':
+			var t = ""
+			ch = self.read_file()
+			while ch != '|':
+				t += ch
+				ch = self.read_file()
+			self.name_str = t
+		elif com_id == '8':
+			var d = ""
+			ch = self.read_file()
+			while ch != '|':
+				d += ch
+				ch = self.read_file()
+			self.delay_text = float(d)
+		elif com_id == '9':
+			var tcsh = ""
+			ch = self.read_file()
+			while ch != '|':
+				tcsh += ch
+				ch = self.read_file()
+			self.text_csh = int(tcsh)
+		elif com_id == 'a':
+			var tcsv = ""
+			ch = self.read_file()
+			while ch != '|':
+				tcsv += ch
+				ch = self.read_file()
+			self.text_csv = int(tcsv)
+		elif com_id == 'b':
+			var ncsh = ""
+			ch = self.read_file()
+			while ch != '|':
+				ncsh += ch
+				ch = self.read_file()
+			self.name_csh = int(ncsh)
+	elif com_id == "1":
+		com_id = ""
+		ch = self.read_file()
+		while ch != '|':
+			com_id += ch
+			ch = self.read_file()
+		if com_id == "0":
+			var bid = ""
+			ch = self.read_file()
+			while ch != '|':
+				bid += ch
+				ch = self.read_file()
+			self.bg.set_texture(load(bg_list[int(bid)]))
+		elif com_id == "1":
+			var sp = ""
+			ch = self.read_file()
+			while ch != '|':
+				sp += ch
+				ch = self.read_file()
+			self.bg_b = true
+			self.bgsp = float(sp)
+		elif com_id == "2":
+			var sp = ""
+			ch = self.read_file()
+			while ch != '|':
+				sp += ch
+				ch = self.read_file()
+			self.bg_l = true
+			self.bgsp = float(sp)
+		elif com_id == "3":
+			var sp = ""
+			ch = self.read_file()
+			while ch != '|':
+				sp += ch
+				ch = self.read_file()
+			self.bg_b = true
+			self.bg_l = true
+			self.bgsp = float(sp)
+		elif com_id == "4":
+			var sp = ""
+			var bid = ""
+			ch = self.read_file()
+			while ch != ',':
+				sp += ch
+				ch = self.read_file()
+			while ch != '|':
+				bid += ch
+				ch = self.read_file()
+			self.bg_b = true
+			self.bg_l = true
+			self.bg_c = true
+			self.bgsp = float(sp)
+			self.bgid = int(bgid)
+	if current_file_text[current_pos_in_file] == '\n':
+		current_pos_in_file += 1
